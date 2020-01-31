319c56c7c8a25ff308c330e00e0be9c96cd44c8d
Kyle Lockwood <58640375+kylelockwood@users.noreply.github.com>
2020-01-30 20:51:03 -0800

    Added output path functionality
    and error checking

--- pdf2png.py
+++ pdf2png.py
@@ -1,25 +1,38 @@
-#! python3
+#!python3
 
 import os
 import subprocess
-import sys
+#import sys
 from glob import glob
 import wx
 
 # Poppler is required for this type of file conversion
-POPPATH = r'C:\Program Files (x86)\Poppler\poppler-0.68.0\bin\pdftoppm.exe'
-DESKTOPPATH = 'C:' + os.environ["HOMEPATH"] + '\\Desktop\\'
-
+POPPATH =       r'C:\Program Files (x86)\Poppler\poppler-0.68.0\bin\pdftoppm.exe'
+HOMEPATH =      'C:' + os.environ["HOMEPATH"]
+DESKTOPPATH =   HOMEPATH + '\\Desktop\\'
+SCRIPTPATH =    HOMEPATH + '\\Documents\\pdf2png\\'
 
 # Define File Drop Target class
 class FileDropTarget(wx.FileDropTarget):
-
+""" Handles the dropped file"""
     def __init__(self, obj):
         # Initialize the wxFileDropTarget Object
         wx.FileDropTarget.__init__(self)
         self.obj = obj
 
-    def on_drop_files(self, x, y, filenames):
+    def OnDropFiles(self, x, y, filenames):
+        # Get path from text box
+        OUTPATH = get_path(app)
+
+        # Check if file output path exists
+        if not os.path.exists(OUTPATH):
+            wx.MessageBox(f'\'{OUTPATH}\' does not exist.  Please specify a valid file destination.', 'Unknown path', wx.OK | wx.ICON_ERROR | wx.STAY_ON_TOP)
+            destBox = app.frame.destinationBox
+            destBox.Clear()
+            destBox.WriteText(read_saved_path())
+            return False
+        
+        # Iterate over all dropped files
         for file in filenames:
             # Create outFile name
             outFile = os.path.basename(file)
@@ -28,57 +41,92 @@ class FileDropTarget(wx.FileDropTarget):
             # Check if valid file type
             if not file.endswith('.pdf'):
                 wx.MessageBox(f'\'{outFile}\' is not a PDF', 'Invalid FIle Type', wx.OK | wx.ICON_ERROR | wx.STAY_ON_TOP)
-                continue            
+                continue
+
             # Check if outFile already exists
-            if glob(DESKTOPPATH + outFile + '*'):
+            if glob(OUTPATH + outFile + '*'):
                 choice = wx.MessageBox(f'\'{outFile}\' already exists.  Would you like to overwrite?', 'Info', wx.YES|wx.NO | wx.ICON_QUESTION | wx.STAY_ON_TOP)
                 if choice == wx.NO:
                     continue
-            # Write file name in box
+
+            # Write file name in main box
             self.obj.WriteText(file + '\n')
 
             # Create image file
             print(f'Converting \'{outFile}\' to png... ', end='', flush=True)
-            # TODO Dynamic output path, default=DESKTOPPATH
-            subprocess.Popen(f'\"{POPPATH}\" -png \"{file}\" \"{DESKTOPPATH}{outFile}\"')
+            subprocess.Popen(f'\"{POPPATH}\" -r 300 -png \"{file}\" \"{OUTPATH}{outFile}\"')
             print('Done')
         return True
 
+
 class MainWindow(wx.Frame):
-    
+    """ Create main app window """
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, wx.ID_ANY, title, size=(287,280), style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
         self.SetBackgroundColour(wx.Colour(240,255,255))
-        
-        # Text box to receive Dropped Files
+        wx.EVT_CLOSE(self, self.OnClose)
+
+        # Create Text box to receive Dropped Files
         wx.StaticText(self, -1, 'Drag and drop PDF(s) here', (10, 10))
         self.textBox = wx.TextCtrl(self, -1, "", pos=(10,30), size=(250,150), style=wx.TE_MULTILINE) #, |wx.HSCROLL|wx.TE_READONLY
         dt = FileDropTarget(self.textBox)
+
         # Link the Drop Target Object to the Text Control
         self.textBox.SetDropTarget(dt)
 
-        # Text box for output file path
-        wx.StaticText(self, -1, 'Output file path:', (10, 188))
-        self.textBox2 = wx.TextCtrl(self, -1, "", pos=(10,208), size=(250,22)) #, |wx.HSCROLL|wx.TE_READONLY
-        self.textBox2.WriteText(DESKTOPPATH)
-
-        # TODO Store user default path, preferences file
+        # CreateText box for output file path
+        wx.StaticText(self, -1, 'File destination:', (10, 188))
+        self.destinationBox = wx.TextCtrl(self, -1, "", pos=(10,208), size=(250,22), style=wx.TE_PROCESS_ENTER) #, |wx.HSCROLL|wx.TE_READONLY
+        self.destinationBox.WriteText(OUTPATH)
 
         # Display the Window
         self.Show(True)
+
+    def OnClose(self, event):
+        # When the app is closed
+        save_file(get_path(app))
+        self.Destroy()
  
-    def close_window(self):
-        self.Close()
- 
+
 class MyApp(wx.App):
+""" Creates the app instance"""
     def OnInit(self):
         # Declare the Main Application Window
-        frame = MainWindow(None, -1, "PDF to PNG")
-        self.SetTopWindow(frame)
+        self.frame = MainWindow(None, -1, "PDF to PNG")
+        self.SetTopWindow(self.frame)
         return True
- 
+
+
+def get_path(app):
+    # Return the current value in destinationBox
+    return app.frame.destinationBox.GetValue()
+
+
+def save_file(content):
+    # Save the output destination
+    if not os.path.exists(content):
+        content = DESKTOPPATH
+    print(f'Saving {content} to outpath.txt')
+    with open(SCRIPTPATH + 'outpath.txt', 'w') as f:
+        f.write(content)
+        f.close()
+    return
+
+
+def read_saved_path():
+    # Get the stored output path from file
+    if not os.path.exists(SCRIPTPATH + 'outpath.txt'):
+        print('file does not exist')
+        with open(SCRIPTPATH + 'outpath.txt', 'w') as f:
+            f.write(DESKTOPPATH)
+    with open(SCRIPTPATH + 'outpath.txt', 'r') as f:
+        outpath = f.read()
+        print(f'outpath : {outpath}')
+        f.close()
+        return outpath
 
  
 # Declare the Application and start the Main Loop
+OUTPATH = read_saved_path()
 app = MyApp(0)
 app.MainLoop()
